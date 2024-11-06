c09e55e9a392fc7396870901287bdf593055cb44
Samuel Barbosa <samuca192silva@hotmail.com>
2024-11-05 21:55:11 -0300

    Refactor - Variable names and calculate

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -6,94 +6,100 @@ import {
   ButtonDanger,
   ButtonSuccess
 } from "styles/buttons"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Calculator() {
-  const [entryHoursValues, setEntryHoursValues] = useState<string[]>(['', ''])
-  const [leaveHoursValues, setLeaveHoursValues] = useState<string[]>(['', ''])
-  const [countHoursValues, setCountHoursValues] = useState<string[]>(['', ''])
-  const [totalHoursValues, setTotalHoursValues] = useState<string>('')
+  const [start, setStart] = useState<string[]>(['', ''])
+  const [finish, setFinish] = useState<string[]>(['', ''])
+  const [interval, setInterval] = useState<string[]>(['', ''])
+  const [total, setTotal] = useState<string>('--:--')
+  const [isRemoving, setIsRemoving] = useState<boolean>(false)
 
-  const handleEntryChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValues = [...entryHoursValues]
+  const handleStartChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>):void => {
+    const newValues = [...start]
     newValues[index] = event.target.value
-    setEntryHoursValues(newValues)
+    setStart(newValues)
   }
 
-  const handleLeaveChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValues = [...leaveHoursValues]
+  const handleFinishChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>):void => {
+    const newValues = [...finish]
     newValues[index] = event.target.value
-    setLeaveHoursValues(newValues)
+    setFinish(newValues)
   }
 
-  const addInput = () => {
-    if (entryHoursValues.length < 5) {
-      setEntryHoursValues([...entryHoursValues, ''])
-      setLeaveHoursValues([...leaveHoursValues, ''])
-      setCountHoursValues([...countHoursValues, ''])
+  const addInput = ():void => {
+    if (start.length < 5) {
+      setStart([...start, ''])
+      setFinish([...finish, ''])
+      setInterval([...interval, ''])
     }
   }
 
-  const removeInput = () => {
-    if (entryHoursValues.length > 1) {
-      const newEntry = [...entryHoursValues]
-      const newLeave = [...leaveHoursValues]
-      const newCount = [...countHoursValues]
-      newEntry.pop()
-      newLeave.pop()
+  const removeInput = ():void => {
+    if (start.length > 1) {
+      const newStart = [...start]
+      const newFinish = [...finish]
+      const newCount = [...interval]
+      newStart.pop()
+      newFinish.pop()
       newCount.pop()
-      setEntryHoursValues(newEntry)
-      setLeaveHoursValues(newLeave)
-      setCountHoursValues(newCount)
+      setStart(newStart)
+      setFinish(newFinish)
+      setInterval(newCount)
+      setIsRemoving(true)
     }
-  };
+  }
 
-  const handleSubmit = () => {
-    const entryDate = new Date()
-    const leaveDate = new Date()
+  const handleCalculate = ():void => {
+    const startInterval = new Date()
+    const finishInterval = new Date()
 
-    entryHoursValues.forEach((value, index) => {
-      entryDate.setHours(+value.substring(0, 2))
-      entryDate.setMinutes(+value.substring(3))
-      entryDate.setSeconds(0)
-      leaveDate.setHours(+leaveHoursValues[index].substring(0, 2))
-      leaveDate.setMinutes(+leaveHoursValues[index].substring(3))
-      leaveDate.setSeconds(0)
+    const newInterval = start.map((value, index) => {
+      const format = value || '00:00';
+      startInterval.setHours(+format.substring(0, 2))
+      startInterval.setMinutes(+format.substring(3))
+      startInterval.setSeconds(0);
+      finishInterval.setHours(+finish[index].substring(0, 2))
+      finishInterval.setMinutes(+finish[index].substring(3))
+      finishInterval.setSeconds(0);
 
-      let diff = (leaveDate.getTime() - entryDate.getTime()) / 1000 / 60;
-
+      let diff = (finishInterval.getTime() - startInterval.getTime()) / 1000 / 60;
       if (diff < 0) {
-        diff += 24 * 60;
+        diff += 24 * 60
       }
 
-      const hours = Math.floor(diff / 60);
-      const minutes = diff % 60;
-      setCountHoursValues(prev => {
-        const newCountHoursValues = [...prev]
-        newCountHoursValues[index] = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
-        return newCountHoursValues;
-      })
+      const hours = Math.floor(diff / 60)
+      const minutes = diff % 60
+
+      return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
     })
 
-    const totalMinutos = countHoursValues.reduce((soma, tempo) => soma + converterParaMinutos(tempo), 0)
-    setTotalHoursValues(converterParaHorasEMinutos(totalMinutos))
+    setInterval(newInterval)
+
+    const totalMinutes = newInterval.reduce((soma, tempo) => soma + toMinutes(tempo), 0)
+    setTotal(toHours(totalMinutes))
   }
 
-  const converterParaMinutos = (tempo: string) => {
-    console.log(tempo)
-    const [horas, minutos] = tempo.split(':').map(Number);
-    return (horas * 60) + minutos;
+  const toMinutes = (timeString: string): number => {
+    const [hours, minutes] = timeString.split(':').map(Number)
+    return (hours * 60) + minutes;
   }
 
-  const converterParaHorasEMinutos = (minutos: number) => {
-    console.log(minutos)
-    const horas = Math.floor(minutos / 60);
-    const minutosRestantes = minutos % 60;
-    return `${String(horas).padStart(2, '0')}:${String(minutosRestantes).padStart(2, '0')}`;
+  const toHours = (minutes: number): string => {
+    const hours = Math.floor(minutes / 60)
+    const rest = minutes % 60
+    return `${String(hours).padStart(2, '0')}:${String(rest).padStart(2, '0')}`
   }
 
-  const disabledAdd = () => entryHoursValues.length > 4
-  const disabledRemove = () => entryHoursValues.length < 2
+  const disabledAdd = ():boolean => start.length > 4
+  const disabledRemove = ():boolean => start.length < 2
+
+  useEffect(() => {
+    if (isRemoving) {
+      handleCalculate()
+      setIsRemoving(false)
+    }
+  }, [isRemoving])
 
   return (
     <>
@@ -101,7 +107,7 @@ export default function Calculator() {
         <p className="text-2xl py-4">Intervalos</p>
         <div className="flex pb-4">
           <div className="flex flex-col">
-            {entryHoursValues.map((value, index) => (
+            {start.map((value, index) => (
               <div key={`label-${index}`} className="flex h-10 items-center">
                 <p key={`text-${index}`} className="w-24 text-lg">Intervalo {index + 1}:</p>
 
@@ -109,7 +115,7 @@ export default function Calculator() {
                   <input
                     className="border border-solid border-gray-500 rounded px-1 w-24"
                     key={`entry-${index}`}
-                    onChange={handleEntryChange(index)}
+                    onChange={handleStartChange(index)}
                     type="time"
                     value={value}
                   />
@@ -118,28 +124,28 @@ export default function Calculator() {
                 <div className="mx-1">
                   <input
                     className="border border-solid border-gray-500 rounded px-1 w-24"
-                    key={`leave-${index}`}
-                    onChange={handleLeaveChange(index)}
+                    key={`finish-${index}`}
+                    onChange={handleFinishChange(index)}
                     type="time"
-                    value={leaveHoursValues[index]}
+                    value={finish[index]}
                   />
                 </div>
                 <div className="mx-1">
                   <input
-                    className={`px-0.5 w-24 bg-white ${countHoursValues[index] ? "visible" : "invisible"}`}
+                    className={`px-0.5 w-24 bg-white ${interval[index] ? "visible" : "invisible"}`}
                     disabled
                     key={`count-${index}`}
                     readOnly
                     type="time"
-                    value={countHoursValues[index]}
+                    value={interval[index]}
                   />
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <p className="text-2xl pb-4">Total de horas: {totalHoursValues}</p>
-        <Button onClick={handleSubmit}>Calcular</Button>
+        <p className="text-2xl pb-4">Total de horas: {total}</p>
+        <Button onClick={handleCalculate}>Calcular</Button>
         <ButtonSuccess disabled={disabledAdd()} className="ml-2" onClick={addInput}>Adicionar intervalo</ButtonSuccess>
         <ButtonDanger disabled={disabledRemove()} className="ml-2" onClick={removeInput}>Remover intervalo</ButtonDanger>
       </Container>
